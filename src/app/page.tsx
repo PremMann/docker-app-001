0bb43edda7e96d1624fe0618bd6fb4f332e34d52
Prem MANN <premmann@GG-2.local>
2025-01-26 19:29:08 +0700

    init v003

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import prisma from "@/lib/db";
 
 export default async function Home() {
   try {
-    const users = await prisma.user.findMany();
+    const users: { id: string; name: string }[] = await prisma.user.findMany();
     return (
       <div>
         <h1>Home Docker prisma </h1>
         {
-          users.map((user: { id: string; name: string }) => (
+          users.map((user) => (
             <div key={user.id}>
               <h2>{user.name}</h2>
             </div>
