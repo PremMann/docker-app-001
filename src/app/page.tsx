fa95f62991e9eebcbdda6b8cfd507e27bcea45d7
Prem MANN <premmann@GG-2.local>
2025-01-26 19:20:41 +0700

    init v002

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import prisma from "@/lib/db";
 
 export default async function Home() {
-  const user = await prisma.user.find
-  return (
-    <div>
-      <h1>Home Docker prisma </h1>
-    </div>
-  );
+  try {
+    const users = await prisma.user.findMany();
+    return (
+      <div>
+        <h1>Home Docker prisma </h1>
+        {
+          users.map((user: { id: string; name: string }) => (
+            <div key={user.id}>
+              <h2>{user.name}</h2>
+            </div>
+          ))
+        }
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return (
+      <div>
+        <h1>Error fetching users</h1>
+      </div>
+    );
+  }
 }
